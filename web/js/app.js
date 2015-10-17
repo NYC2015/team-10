ac456ecdf885645d6ba63499f7dac4e6cf60dbf1
Michael <mzc2fd@virginia.edu>
2015-10-17 01:47:04 -0400

    some more angular and trying out firebase

--- web/js/app.js
+++ web/js/app.js
@@ -1,24 +1,40 @@
 var mainApp = angular.module("mainApp", ['ngRoute']);
          
-mainApp.controller("HelloController", function($scope) {
-	$scope.helloTo = {};
-	$scope.helloTo.title = "AngularJS";
+mainApp.controller('eventController', function($scope) {
+	$scope.message = "This page will be used to display add student form";
+});      
+mainApp.controller('somethingController', function($scope) {
+	$scope.message = "This page will be used to display all the students";
 });
-/*
+
 mainApp.config(['$routeProvider', function($routeProvider) {
-   $routeProvider.
-   when('/'. {
-	   templateUrl: 'index.html', controller: 'homeController'
-   }).
-   when('/event', {
-      templateUrl: 'event.html', controller: 'AddStudentController'
-   }).
-   when('/something', {
-      templateUrl: 'something.html', controller: 'ViewStudentsController'
-   }).
-   otherwise({
-      redirectTo: '/'
-   });
+	$routeProvider.
+	when('/event', {
+	  templateUrl: 'event.html', controller: 'eventController'
+	}).
+	when('/something', {
+	  templateUrl: 'something.html', controller: 'somethingController'
+	}).
+	otherwise({
+	  redirectTo: 'index.html'
+	});
 	
 }]);
-*/
+//var myDataRef = new Firebase('https://blinding-heat-105.firebaseio.com');
+var myDataRef = new Firebase('https://ugvkexm8jyc.firebaseio-demo.com/');
+$('#messageInput').keypress(function (e) {
+	if (e.keyCode == 13) {
+		var name = $('#nameInput').val();
+		var text = $('#messageInput').val();
+		myDataRef.push({name: name, text: text});
+		$('#messageInput').val('');
+	}
+});
+myDataRef.on('child_added', function(snapshot) {
+	var message = snapshot.val();
+	displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+	$('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+	$('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
