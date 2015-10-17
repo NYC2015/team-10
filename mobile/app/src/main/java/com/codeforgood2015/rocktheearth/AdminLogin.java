319b6a8f9d8717762d83910cfe54fbcf8ad59c86
josephbaik <josephbaik@gmail.com>
2015-10-16 22:47:51 -0400

    buttons and input types

--- mobile/app/src/main/java/com/codeforgood2015/rocktheearth/AdminLogin.java
+++ mobile/app/src/main/java/com/codeforgood2015/rocktheearth/AdminLogin.java
@@ -1,5 +1,6 @@
 package com.codeforgood2015.rocktheearth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class AdminLogin extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class AdminLogin extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdminLogin.this, RegisterPage.class);
+                AdminLogin.this.startActivity(i);
             }
         });
     }
